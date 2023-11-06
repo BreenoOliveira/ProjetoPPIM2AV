08d95b4a2451458d4c1aec9ceb641bc3ac72dfcd
BreenoOliveira <brenooliveira2817@gmail.com>
2023-11-06 20:45:28 -0300

    feats: Alteração na estilização do formulario.

--- index.js
+++ index.js
@@ -9,32 +9,32 @@ function processarCadastroUsuario(requisicao, reposta){
     const usuario = {
         nome: requisicao.query.nome,
         sobrenome: requisicao.query.sobrenome,
-        nomeUsuario: requisicao.query.nomeUsuario,
         cidade: requisicao.query.cidade,
         uf: requisicao.query.uf,
-        cep: requisicao.query.cep,
+        curso: requisicao.query.curso,
+        obs: requisicao.query.obs,
     } 
     listaUsuarios.push(usuario);
 
     let conteudoResposta =`
     <!DOCTYPE html>
-        <html lang="en">
+        <html lang="pt-br">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>Alunos Cadastrados</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         </head>
         <body>
-            <h1>Lista de usuários cadastrados</h1>
+            <h1 style="color: green; text-align: center;">Lista de Alunos cadastrados</h1>
             <table class="table table-striped table-hover">
                 <thead>
                     <tr>
                         <th>Nome</th>
                         <th>Sobrenome</th>
-                        <th>Nome de Usuário</th>
                         <th>Cidade/UF</th>
-                        <th>CEP</th>
+                        <th>Curso</th>
+                        <th>Observação</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -45,9 +45,9 @@ function processarCadastroUsuario(requisicao, reposta){
                         <tr>
                             <td>${usuario.nome}</td>
                             <td>${usuario.sobrenome}</td>
-                            <td>${usuario.nomeUsuario}</td>
                             <td>${usuario.cidade}/${usuario.uf}</td>
-                            <td>${usuario.cep}</td>
+                            <td>${usuario.curso}</td>
+                            <td>${usuario.obs}</td>
                         <tr>
                     `;
                 }
@@ -55,8 +55,8 @@ function processarCadastroUsuario(requisicao, reposta){
                 conteudoResposta += `
                             </tbody>
                         </table>
-                        <a class="btn btn-primary" href="/" role"button"> Voltar ao menu </a>
-                        <a class="btn btn-primary" href="/cadastroUsuario.html" role"button"> Continuar cadastrando </a>
+                        <a class="btn btn-danger" href="/" role"button"> Voltar ao menu </a>
+                        <a class="btn btn-success" href="/cadastroUsuario.html" role"button"> Continuar cadastrando </a>
                     </body>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script
                     </html>
@@ -71,17 +71,20 @@ app.use(express.static('./paginas'));
 app.get('/', (requisicao, reposta) =>{
     reposta.end (`
     <!DOCTYPE html>
-        <html lang="en">
+        <html lang="pt-br">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Menu do Sistema</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         </head>
         <body>
+        <div class="col-4">
             <h1>Menu</h1>
-            <ul>
-                <li><a href="/cadastroUsuario.html">Cadastrar Usuario</a></li>
-            </ul>
+            <nav class="nav flex-column">
+                <a style="font-size: 30px;" class="nav-link" href="/cadastroUsuario.html">Cadastrar Aluno</a>
+            </nav>
+        </div>    
         </body>
         `);
 })
